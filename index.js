3b3c391785dff497bcf990dc981c51bf082979f6
vlxa <vit.lexa@gmail.com>
2021-11-18 19:07:08 +0100

    /api/persons/:id implemented

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ app.get('/info', (request, response) => {
   response.send(message)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = +request.params.id
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.statusMessage = `Person "${id}" does not exist.`
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
